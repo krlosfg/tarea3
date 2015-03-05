dfdeadbd5664093a7da242f3b633b5b3bb3839f4
krlosfg <krlosfg23@gmail.com>
2015-03-04 23:42:13 -0600

    "Tarea3_#201021083"
    tarea3

---
+++ src/TAREA3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class TAREA3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer n;
+Scanner sc= new Scanner (System.in);
+  System.out.print ("Introdusca un Numero");
+		n = sc. nextInt();
+		if (n%2==0);
+			System.out.print("Es un numero par");
+	
+	else
+			System.out.print("es un numero impar");
+		
+
+	}
+
+}
